78350ab0bfab93be6f9230a53f580cacf05d9375
853 <sonic853@qq.com>
2021-02-09 11:33:20 +0800

    1.2.7

--- BiliAlbumDown_greasyforkVer.user.js
+++ BiliAlbumDown_greasyforkVer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         哔哩哔哩图片打包下载（支持相簿和专栏
-// @version      1.2.6
+// @version      1.2.7
 // @description  下载B站UP主Bilibili动态相册相簿图片，以及视频封面，专栏图片和UP主头像以及主页壁纸，直播间封面和直播间壁纸，然后提交给aria2或打包成zip
 // @author       Sonic853
 // @namespace    https://blog.853lab.com
@@ -742,7 +742,7 @@
                                     Console_Devlog(rs)
                                     rs.forEach(ce => {
                                         // if (ce.startsWith("//")) {
-                                            this.add_img("https:" + ce.split("//")[1], e.id, cou)
+                                            this.add_img("https://" + ce.split("//")[1].slice(0, -1), e.id, cou)
                                         // } else if (ce.startsWith("http:") || ce.startsWith("https:")) {
                                         //     this.add_img(ce, e.id, cou)
                                         // } else {
