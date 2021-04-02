70a251270984f4c604455b30776df5de052833b2
853 <sonic853@qq.com>
2021-04-02 13:39:13 +0800

    Update Kaiheila_GetKey.user.js

--- Kaiheila_GetKey.user.js
+++ Kaiheila_GetKey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         开黑啦Key获取
 // @namespace    https://853lab.com/
-// @version      0.2
+// @version      0.3
 // @description  获取key用于开发用途。
 // @author       Sonic853
 // @match        https://kaiheila.cn/app/*
@@ -12,14 +12,20 @@
 (function() {
     'use strict';
     // Your code here...
-    GM_registerMenuCommand("获取key",()=>{
-        let auth = JSON.parse(localStorage.getItem("auth"));
+    GM_registerMenuCommand("开黑啦BOT",()=>{
+        const auth = JSON.parse(localStorage.getItem("auth"));
         if(auth!=null){
-            let j = {
+            const j = {
                 auth:auth.authorization,
                 token:auth.rong_token
             }
             prompt("复制以下的信息到开黑啦BOT里，请勿将这些内容暴露在公共场合下",JSON.stringify(j));
+        }
+    });
+    GM_registerMenuCommand("KaiheilaWS",()=>{
+        const auth = JSON.parse(localStorage.getItem("auth"));
+        if(auth!=null){
+            prompt("复制以下的信息到KaiheilaWS里，请勿将这些内容暴露在公共场合下",auth.authorization);
         }
     });
 })();
