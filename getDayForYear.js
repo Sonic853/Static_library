91080241cd6c349cc481583755ecec7c5ef6ac19
Sonic853 <sonic853@qq.com>
2023-03-27 10:53:14 +0800

    getDays

---
+++ getDayForYear.js
@@ -0,0 +1,11 @@
+
+// 求今年过去了多少天
+const getDays = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const start = new Date(year, 0, 0);
+    const diff = now - start;
+    const oneDay = 1000 * 60 * 60 * 24;
+    const day = Math.floor(diff / oneDay);
+    return day;
+}
